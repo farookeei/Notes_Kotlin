857e212eac5b571ed3302c0529b75f10482f4da7
Farook Jamal <farookyj10@gmail.com>
2026-06-15 16:29:01 +0530

    feat: initialize iOS project setup and configure Room database constructor for KMP support

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by farook on 15/06/26.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
